38388da7f2c3578f22ea44c42b01fc9139f1136b
Jaann <hamizanputra321@gmail.com>
2025-09-08 23:09:49 +0700

    tamatin misi 7 sept

--- 7 sept/whileLoop.java
+++ 7 sept/whileLoop.java
@@ -3,8 +3,18 @@ public class whileLoop {
         int i = 10;
 
         while (i >= 1){
-            System.out.println(i);
+            System.out.print(i + " ");
             i--;
+        }
+
+        System.out.print("\n");
+
+        int hp = 100;
+        while (hp > 0){
+            int angkaRandom = (int)(Math.random() * 11) + 1;
+            hp -= angkaRandom;
+            System.out.println("HP tersisa: " + hp);
+            if (hp < 0) hp = 0;
         }
     }
 }
