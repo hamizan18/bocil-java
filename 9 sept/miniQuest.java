cb40971484492b24fed07646dc03822f8e9daf71
Jaann <hamizanputra321@gmail.com>
2025-09-11 12:56:02 +0700

    harusnya dah dri kemaren, rupanya bug

--- 9 sept/miniQuest.java
+++ 9 sept/miniQuest.java
@@ -1,8 +1,39 @@
+import java.util.Scanner;
+
 public class miniQuest {
     public static void main(String[] args){
-        int hp = 100;
-        int damageRandom = (int)(Math.random() * 11) + 1;
+        try (Scanner input = new Scanner(System.in)){
+            int hp = 100;
+            int enemyHp = 50;
+            int choice = 0;
+            
+            while (hp > 0 && enemyHp > 0){
+                int damageRandom = (int)(Math.random() * 11) + 1;
+                int healing = (int)(Math.random() * 5) + 1;
 
-        
+                System.out.print("1. Attack\n2. Heal\n3. Run\nChoose your action: ");
+                choice = input.nextInt();
+
+                switch(choice){
+                    case 1: 
+                        enemyHp -= damageRandom; 
+                        hp -= damageRandom;
+                        if (hp < 0) hp = 0;
+                        if (enemyHp < 0) enemyHp = 0;
+                        System.out.println("hp Player: " + hp); 
+                        System.out.println("hp Enemy: " + enemyHp);
+                        break;
+                    case 2: 
+                        hp += healing;
+                        System.out.println("hp Player increased!\n Player hp: " + hp);
+                        break;
+                    case 3:
+                        System.out.println("Leaving war..");
+                        return;
+                    default: System.out.println("Choose 1 - 3!");
+                        return;
+                }
+            }
+        }
     }
 }
