aad7df4e16fb26ee80d0ba98aae5394ae66b66a4
Jaann <hamizanputra321@gmail.com>
2025-09-12 10:28:35 +0700

    daily quest 1 - 12 sept

---
+++ 12 sept/mainQuest.java
@@ -0,0 +1,21 @@
+public class mainQuest{
+    public static void main(String[] args){
+        String[] inventory = {"Potion", "Sword", "Shield"};
+        int i;
+
+        for (i = 0;i < inventory.length;i++){
+            System.out.printf("\nItem %d dari inventory: %s", i+1, inventory[i]);
+        }
+
+        String[] addInventory = new String[inventory.length + 1];
+        for (i = 0;i < inventory.length;i++){
+            addInventory[i] = inventory[i];
+        }
+        addInventory[addInventory.length - 1] = "Bow";
+
+        System.out.println("\n\nAfter adding bow: ");
+        for (i = 0;i < addInventory.length;i++){
+            System.out.printf("\nItem %d dari inventory: %s", i+1, addInventory[i]);
+        }
+    }
+}
