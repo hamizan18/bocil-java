a73fdc71bc99ccbb0d78a77215c3cc79b2d1d579
Jaann <hamizanputra321@gmail.com>
2025-09-18 19:28:25 +0700

    kelamaan disini

--- 14 sept/classnObject.java
+++ 14 sept/classnObject.java
@@ -8,8 +8,13 @@ class Hero {
         enemy.hp -= attackPower;
         return enemy.hp;
     }
+    int heal(int amount) {
+        System.out.println("Healing..[+" + amount + "hp]");
+        hp += amount;
+        return hp;
+    }
     void showStatus() {
-        System.out.println("Player HP: " + hp + " | Attack: " + attackPower);
+        System.out.println("Player HP: " + hp + "\t| Attack: " + attackPower);
     }
 }
 class Enemy {
@@ -23,7 +28,7 @@ class Enemy {
         return hero.hp;
     }
     void showStatus() {
-        System.out.println("HP: " + hp + " | Attack: " + attackPower);
+        System.out.println("Enemy HP: " + hp + "\t| Attack: " + attackPower);
     }
 }
 public class classnObject {
@@ -32,16 +37,36 @@ public class classnObject {
         hero1.name = "Mingjaan";
         hero1.hp = 100;
         hero1.attackPower = 15;
+        int amount = (int)(Math.random()* 8) + 12;
+
+        Hero hero2 = new Hero();
+        hero2.name = "Hemroo";
+        hero2.hp = 30;
+        hero2.attackPower = (int)(Math.random()* 10) + 25;
 
         Enemy enemy1 = new Enemy();
         enemy1.name = "Goblin";
         enemy1.hp = 80;
-        enemy1.attackPower = 12;
+        enemy1.attackPower = 28;
 
+        System.out.println("--- BEFORE WAR ---");
         hero1.showStatus();
-        hero1.attack(enemy1);
-
+        hero2.showStatus();
         enemy1.showStatus();
-        enemy1.attack(hero1);
+
+        System.out.println("\n[HERO]");
+        enemy1.hp = hero1.attack(enemy1);
+        hero1.heal(amount);
+        enemy1.hp = hero2.attack(enemy1);
+
+        System.out.println("\n[ENEMY]");
+        hero1.hp = enemy1.attack(hero1);
+        hero2.hp = enemy1.attack(hero2);
+
+        System.out.println("\n--- AFTER WAR ---");
+        hero1.showStatus();
+        hero2.showStatus();
+        enemy1.showStatus();
+
     }
 }
